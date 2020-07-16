a53e16635345a90142c39b012305341feb9555db
Abigail Swigert <abigail@swigertdesigns.com>
2020-07-16 13:44:06 -0500

    passed test for 1 taxable item total tax due, failed 1 tax exempt item total tax due

--- TDDexercise/src/com/java/ReceiptGenerator.java
+++ TDDexercise/src/com/java/ReceiptGenerator.java
@@ -10,14 +10,23 @@ public class ReceiptGenerator {
         };
         Receipt receipt = new Receipt();
         receipt.totalDue = calcTotalDue(basket);
+        receipt.totalTax = calcTotalTax(basket);
         return receipt;
     }
 
     private static double calcTotalDue(Item[] basket) {
-        double totalDue = 0;
+        double totalDue = 0.0;
         for (Item item: basket) {
             totalDue += item.price;
         }
         return totalDue;
+    }
+
+    private static double calcTotalTax(Item[] basket) {
+        double totalTax = 0.0;
+        for (Item item: basket) {
+            totalTax += (item.price * 1.10);
+        }
+        return totalTax;
     }
 }
