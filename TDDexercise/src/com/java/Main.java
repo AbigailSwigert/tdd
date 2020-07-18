8fba76459338883612756beefec319f0ea77181e
Abigail Swigert <abigail@swigertdesigns.com>
2020-07-18 11:40:26 -0500

    added input 3, output 3 is inncorrect

--- TDDexercise/src/com/java/Main.java
+++ TDDexercise/src/com/java/Main.java
@@ -14,10 +14,20 @@ public class Main {
 
         // Input 2:
         Item importedBoxOfChocolates1 = new Item(1, "imported box of chocolates", 10.00, true, true);
-        Item importedBottleOfPerfume = new Item(1, "imported bottle of perfume", 47.50, false, true);
+        Item importedBottleOfPerfume1 = new Item(1, "imported bottle of perfume", 47.50, false, true);
 
-        Item[] basket2 = new Item[]{importedBoxOfChocolates1, importedBottleOfPerfume};
+        Item[] basket2 = new Item[]{importedBoxOfChocolates1, importedBottleOfPerfume1};
 
         ReceiptGenerator.generateReceipt(basket2);
+
+        // Input 3:
+        Item importedBottleOfPerfume2 = new Item(1, "imported bottle of perfume", 27.99, false, true);
+        Item bottleOfPerfume = new Item(1, "bottle of perfume", 18.99, false, false);
+        Item packetOfHeadachePills = new Item(1, "packet of headache pills", 9.75, true, false);
+        Item importedBoxOfChocolates2 = new Item(1, "imported box of chocolates", 11.25, true, true);
+
+        Item[] basket3 = new Item[]{importedBottleOfPerfume2, bottleOfPerfume, packetOfHeadachePills, importedBoxOfChocolates2};
+
+        ReceiptGenerator.generateReceipt(basket3);
     }
 }
